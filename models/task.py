709b51fca9aa30e427a5ed08caef33c78a86a7db
AhmedHr22 <ahmed.work.2200@gmail.com>
2024-10-02 15:03:58 +0100

    task1 after validation

--- models/task.py
+++ models/task.py
@@ -1,5 +1,6 @@
 from odoo import api,fields,models
 from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class Task(models.Model):
     _name="task.managements"
@@ -10,8 +11,7 @@ class Task(models.Model):
     title = fields.Char(required=True,string="Titre")
     description = fields.Text(required=True,string="Description")
     due_date = fields.Date(required=True,string="Date Echeance") 
-   
-    delay = fields.Boolean(string="Retard")
+    delay = fields.Boolean(string="Retard",compute='compute_delay')
     priority = fields.Selection(
         [('basse','Basse'),
          ('moyenne','Moyenne'),
@@ -19,7 +19,6 @@ class Task(models.Model):
          default="basse",
          required=True,
          string="Priorite")
-   
     patern = fields.Char(required=True,string='Motif')
     state = fields.Selection(
         [('broullion','Broullion'),
@@ -28,31 +27,35 @@ class Task(models.Model):
          default='broullion',
          required=True,
          string="State")
-    
     category_id = fields.Many2one('category.managements',string="Categorie",required=True)
     
 
     def start(self):
         for rec in self:
             rec.state = "en_cour"
+ 
     
     def in_draft(self):
         for rec in self:
             rec.state = "broullion"
 
+
     def conclude(self):
         for rec in self:
             rec.state = "terminer"
 
+
     def unlink(self):
         for rec in self:
             if rec.state != "broullion":
-                raise ValidationError("pour supprimer une tache il fau que l etat soit en broullion")
+                raise ValidationError("Pour supprimer une tache il faut que l'etat soit en broullion")
             
         return super(Task,self).unlink()
     
-
-
+    @api.depends('due_date')
+    def compute_delay(self):
+            for rec in self:
+                rec.delay = datetime.today().date() > rec.due_date
 
 
 
