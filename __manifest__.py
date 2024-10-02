3b7ad563006c6c6d171cdc90f66d02c4f6a51a10
AhmedHr22 <ahmed.work.2200@gmail.com>
2024-10-02 10:34:53 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"task managements 2",
+    "category":"Sales",
+    "application":True,
+    "installable":True,
+    "license":"LGPL-3",
+    "data":[
+        'security/ir.model.access.csv',
+        'views/category.xml',
+        'views/task.xml',
+        'views/menuitems.xml',
+        'data/category.managements.csv',
+    ],
+    "depends":["base"]
+}
