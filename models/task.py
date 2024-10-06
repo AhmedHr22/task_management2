86709001d269fdf78628e33c1da5fd4ef439f8a7
AhmedHr22 <ahmed.work.2200@gmail.com>
2024-10-06 10:50:02 +0100

    task4 before validation

--- models/task.py
+++ models/task.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     _description = """
     a module for task management
     """
+    _rec_name = "title"
 
     title = fields.Char(required=True,string="Titre")
     description = fields.Text(required=True,string="Description")
@@ -27,6 +28,7 @@ class Task(models.Model):
          default='broullion',
          required=True,
          string="State")
+    # task4
     category_id = fields.Many2one('category.managements',string="Categorie",required=True)
     
 
@@ -61,6 +63,34 @@ class Task(models.Model):
                     rec.delay = False
     # task2
     user_id = fields.Many2one('res.users',string="Utilisateur",default=lambda self:self.env.user)
+
+    # task4
+    is_category_travail = fields.Boolean(default=False,compute="compute_category_id")
+    @api.depends("category_id")
+    def compute_category_id(self):
+        category_travail = self.env.ref('task_management_2.category_record')
+        for rec in self:
+            import logging
+            logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> info >>>>>>>>>>>>>>>>>>>>>>category_id {rec.category_id} \n\n')
+            logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> info >>>>>>>>>>>>>>>>>>>>>>category_travail {category_travail} \n\n')
+
+            rec.is_category_travail = category_travail == rec.category_id
+            
+    linked_task = fields.Many2one('task.managements',string="Tache depandante")
+    @api.constrains('linked_task')
+    def check_linked_task(self):
+        import logging
+        logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> info >>>>>>>>>>>>>>>>>>>>>>linked_task_state {self.linked_task.state} \n\n')
+        logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> info >>>>>>>>>>>>>>>>>>>>>>state {self.state} \n\n')
+
+        if self.linked_task.state == "terminer" and self.state == "broullion" :
+            self.start()  
+        elif not self.linked_task:
+            import logging
+            logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> info >>>>>>>>>>>>>>>>>>>>>>linked task_null : {self.linked_task} \n\n')
+        else:    
+            raise ValidationError("la tache depandante elle doit etre terminer pour commencer la tache courante")
+
 
     
 
